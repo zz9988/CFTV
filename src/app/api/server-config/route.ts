d97b3c1eb02616cae3d9ddeeef6edac4f778c7fd
shinya <kobayashi_shinya@outlook.com>
2025-08-14 17:48:57 +0800

    feat: add current version to /api/server-config

--- src/app/api/server-config/route.ts
+++ src/app/api/server-config/route.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 import { NextRequest, NextResponse } from 'next/server';
+import { CURRENT_VERSION } from '@/lib/version'
 
 import { getConfig } from '@/lib/config';
 
@@ -13,6 +14,7 @@ export async function GET(request: NextRequest) {
   const result = {
     SiteName: config.SiteConfig.SiteName,
     StorageType: process.env.NEXT_PUBLIC_STORAGE_TYPE || 'localstorage',
+    Version: CURRENT_VERSION,
   };
   return NextResponse.json(result);
 }
